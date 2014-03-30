53f87bf603543a4421b65e36630a0c51b4fb3679
Atin <atinruia@gmail.com>
2014-03-29 23:32:13 -0500

    Adding queue code

---
+++ MP4/queue_using_array.C
@@ -0,0 +1,67 @@
+#include "queue.H"
+
+/*Implementation of Queue for Thread Scheduler*/
+
+/*	
+	typedef struct list{
+        	Thread *thread;
+        	struct list *next;
+	};
+
+	struct list **Q;
+*/
+	
+Queue::Queue():head(-1),tail(-1)
+{}
+
+
+int Queue::isEmpty()
+{
+   return (tail == -1);
+}
+
+
+int Queue::isFull()
+{
+   return (((tail+1)%QUEUE_SIZE) == head);
+}
+
+void Queue::enqueue(Thread *th)
+{
+   
+   if (!this->isFull())
+   {
+      if (this->isEmpty())
+         ++head;
+
+      tail = (tail + 1)%QUEUE_SIZE;
+     list[tail] = th;
+   }
+   else
+     printf("\n ReadyQueue Full!!");
+}
+
+
+Thread* Queue::dequeue()
+{
+   if (!this->isEmpty())
+   {
+      Thread *t = list[head];
+      
+      if (head == tail)
+      {
+         tail = -1;
+         head = -1;
+      }
+      else
+         head = (head + 1)%QUEUE_SIZE;
+      
+      return t;
+   }
+   else
+   {
+      printf("\nReadyQueue Empty!!");
+      return NULL;
+   }
+}
+
