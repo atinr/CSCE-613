db9e023acb5d1f693756c5ced8554ccdd6a7a914
AMEY <amey@amey-Inspiron-5720.(none)>
2014-04-26 23:15:31 -0500

    freelist2

--- MP5/freeList.C
+++ MP5/freeList.C
@@ -1,17 +1,28 @@
 #include "freeList.H"
 
+
 freeList::freeList()
 {
+	int i;
+	for(i=0;i<Block_Count/8;i++)
+	{
+		vector[i]=0;
+	}
 }
 
 int freeList::getBlock()
 {
-	int i;
-	for(i=0;i<Block_Count;i++)
+	int i,j;
+	for(i=0;i<Block_Count/8;i++)
 	{
-		if(vector[i]==false){
-			vector[i] = true;
-			return i+1;
+		if(vector[i]!=0xFF){
+			for(j=0;j<8;j++)
+			{
+				if((vector[i]>>(7-j))&1==0)
+				{
+					return 8*i+j+1;
+				}
+			}
 		}
 	}
 	return -1;
@@ -19,6 +30,6 @@ int freeList::getBlock()
 
 void freeList::releaseBlock(int _block_no)
 {
-	vector[_block_no - 1] = false;
+	vector[(_block_no - 1)/8] = vector[(_block_no - 1)/8]|(1<<(7-((_block_no-1)%8)));
 	return;
 }
