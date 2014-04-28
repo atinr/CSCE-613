a217df9bce677b2d0897cea5febba8a7a0acce5d
anil2888 <anilalagiya@gmail.com>
2014-04-28 00:53:43 -0500

    Adding fat and file system

---
+++ MP5/fat.C
@@ -0,0 +1,58 @@
+/* 
+    File: frame_pool.C
+
+    Author: R. Bettati
+            Department of Computer Science
+            Texas A&M University
+    Date  : 09/05/13
+
+    Implementation of the manager for the Free-Frame Pool.
+
+    THIS IS A DUMMY IMPLEMENTATION ONLY! 
+    IT HAS BEEN IMPLEMENTED AS POORLY AS HUMANLY POSSIBLE.
+    DO NOT TRY TO USE OR IMITATE IN ANY WAY!!
+
+    NOTE: THIS IMPLEMENTATION SUPPORTS THE CREATION OF ONLY ONE FRAME POOL!!
+
+*/
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "utils.H"
+#include "fat.H"
+
+/*--------------------------------------------------------------------------*/
+/* LOCAL VARIABLES */
+/*--------------------------------------------------------------------------*/
+
+static unsigned long next_free_frame;
+
+/*--------------------------------------------------------------------------*/
+/* F r a m e   P o o l  */
+/*--------------------------------------------------------------------------*/
+
+FAT::FAT (int _no_blocks){
+    no_blocks = _no_blocks;
+    for(i=0;i<no_blocks;i++){
+        fat[i]=-1; // - 1 means that it is free block; it points to nothing
+    }
+    
+    void FAT::SetNextBlock(int CurrBlock, int NextBlock){
+    
+        fat[CurrBlock] = NextBlock;
+    }
+
+    int GetNextBlock(int CurrBlock){
+        return fat[currBlock];
+    }
+    
+    void SetLastBlock(int CurrBlock){
+        fat[CurrBlock] = 0;
+    }
+    
+    BOOLEAN isLastBlock(int CurrBlock){
+        return (fat[CurrBlock] == 0);
+    }
+}
---
+++ MP5/file_system.C
@@ -0,0 +1,141 @@
+/* 
+    File: file_system.H
+
+    Author: R. Bettati
+            Department of Computer Science
+            Texas A&M University
+    Date  : 10/04/05
+
+    Description: File System.
+    
+
+*/
+
+
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "utils.H"
+#include "file_system.H"
+
+/*--------------------------------------------------------------------------*/
+/* DATA STRUCTURES */ 
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* FORWARD DECLARATIONS */ 
+/*--------------------------------------------------------------------------*/
+
+class FileSystem;
+
+/*--------------------------------------------------------------------------*/
+/* F i l e */
+/*--------------------------------------------------------------------------*/
+
+class File {
+private: 
+     /* -- DEFINE YOUR FILE DATA STRUCTURES HERE. */
+     
+     FileSystem   * file_system;
+
+     unsigned int   file_id;
+
+     /* -- You may want to store other information, such as 
+             .. position in the file
+             .. cached block(s)
+             .. etc.
+      */
+
+public:
+
+    File();
+    /* Constructor for the file handle. Set the 'current
+       position' to be at the beginning of the file. */
+
+    unsigned int Read(unsigned int _n, char * _buf);
+    /* Read _n characters from the file starting at the 
+       current location and copy them in _buf.
+       Return the number of characters read. */
+
+    unsigned int Write(unsigned int _n, char * _buf);
+    /* Write _n characters to the file starting at the current 
+       location, if we run past the end of file, we increase 
+       the size of the file as needed. 
+     */
+
+    void Reset();
+    /* Set the 'current position' at the beginning of the file. */
+
+    void Rewrite();
+    /* Erase the content of the file. Return any freed blocks. 
+       Note: This function does not delete the file! It just erases its
+       content. */
+
+    BOOLEAN EoF();
+    /* Is the current location for the file at the end of the file? */
+
+
+};
+
+/*--------------------------------------------------------------------------*/
+/* F i l e S y s t e m  */
+/*--------------------------------------------------------------------------*/
+
+FileSystem::FileSystem(){
+    int i;
+    
+    for(i=0; i<512; i=i+1){
+        file_list[i].file_id = -1; // Initialize it to -1 ; file has not created.
+    }
+}
+
+BOOLEAN FileSystem::LookupFile(int _file_id, File *_file){
+    BOOLEAN found=0;
+    int i;
+    
+    for(i=0; i<511; i=i+1){
+        if(file_list[i].file_id == _file_id){
+            found = 1;
+            break;
+        }
+    }
+    
+    if(found){
+        _file = &file_list[i];
+        return found;
+        }
+    else{
+        return found;
+    }
+}
+
+BOOLEAN FileSystem::CreateFile(int _file_id){
+    
+    int i;
+    File * _file;
+    BOOLEAN Exist = LookupFile(_file_id, _file);
+    if(Exist){
+        return false;
+    }
+    else{
+        for(i=0; i<511; i=i+1){
+            if(file_list[i].file_id == -1);
+            break;
+        }
+        
+        // Found the next empty spot in directory and i is the index for that
+        file_list[i].file_id = _file_id;
+        // Can not acces the file_id variable... have to define two funtions..set fid and get fid
+        return true;
+    }
+}
