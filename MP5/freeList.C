3dd112315aac260ee8c0f7c614933ba6b6bf9ab7
Atin <atinruia@gmail.com>
2014-04-29 16:11:12 -0500

    adding filesystem structures

--- MP5/freeList.C
+++ MP5/freeList.C
@@ -1,26 +1,29 @@
 #include "freeList.H"
 
-
-freeList::freeList()
+freeList::freeList(char* _vector, int _block_count, bool init):vector(_vector), Block_Count(_block_count)
+//freeList::freeList()
 {
 	int i;
-	for(i=0;i<Block_Count/8;i++)
-	{
-		vector[i]=0;
-	}
+   if (init)
+   {
+	   for( i = 0; i < Block_Count / 8 ;i++)
+	   {
+		   vector[i]= 0 ;
+	   }
+   }
 }
 
 int freeList::getBlock()
 {
 	int i,j;
-	for(i=0;i<Block_Count/8;i++)
+	for( i= 0 ; i < Block_Count / 8 ;i++)
 	{
-		if(vector[i]!=0xFF){
-			for(j=0;j<8;j++)
+		if(vector[i]!= 0xFF ){
+			for( j = 0 ; j < 8 ; j++ )
 			{
-				if((vector[i]>>(7-j))&1==0)
+				if(((vector[i]>>( 7 - j )) & 1) == 0 )
 				{
-					return 8*i+j+1;
+					return 8 * i  +  j + 1;
 				}
 			}
 		}
@@ -32,4 +35,9 @@ void freeList::releaseBlock(int _block_no)
 {
 	vector[(_block_no - 1)/8] = vector[(_block_no - 1)/8]|(1<<(7-((_block_no-1)%8)));
 	return;
+}
+
+bool freeList::allocBlock(int _block_no)
+{
+   
 }
